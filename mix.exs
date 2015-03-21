5f65be73aa53107015e697e38b7bc066a803495c
Alexei Sholik <alcosholik@gmail.com>
2015-03-21 18:18:19 +0200

    Add Hex metadata

--- mix.exs
+++ mix.exs
@@ -6,6 +6,27 @@ defmodule MixErlangTasks.Mixfile do
       app: :"mix-erlang-tasks",
       version: "0.1.0",
       elixir: "~> 1.0",
+      description: description,
+      package: package,
     ]
   end
+
+  defp description do
+    "This project provides a few Mix tasks that make it more convenient to use Mix " <>
+    "as a build tool and package manager when developing applications in Erlang."
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README.md", "LICENSE"],
+      contributors: ["Alexei Sholik"],
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/alco/mix-erlang-tasks",
+      }
+    ]
+  end
+
+  # no deps
+  # --alco
 end
